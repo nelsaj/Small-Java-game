879e76ca8385262aaf94cc94d8f64ab98c75292c
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2024-12-23 14:40:22 +0100

    upddate

--- view/MainView.java
+++ view/MainView.java
@@ -1,14 +1,18 @@
 package view;
 
 import javax.swing.JFrame;
+import view.StartView;
 
 public class MainView extends JFrame {
+    
     
     public MainView(){       
         super("Game");
         this.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 700 );
         this.setVisible(true);
-        
+        this.setResizable(false);
+        this.setLayout(null);
+        new StartView(this).createStartView();
     }
 }
