b9174065cfd661b15091e6472035c1bc14b5d52e
nelsaj <112635402+nelsaj@users.noreply.github.com>
2024-12-26 10:18:50 +0100

    New

--- model/Player.java
+++ model/Player.java
@@ -4,4 +4,27 @@ public class Player {
     private int lives;
     private int score;
     private int playerNbr;
+
+    Player(int playerNbr) {
+
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+    public int getLives() {
+        return lives;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setPlayerNbr(int playerNbr) {
+        this.playerNbr = playerNbr;
+    }
+    public int getPlayerNbr() {
+        return playerNbr;
+    }
 }
---
+++ view/EventView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class EventView implements MessageViewsInterface {
+    @Override
+    public void showMessage(String message) {
+        // TODO Auto-generated method stub
+        
+    }
+}
