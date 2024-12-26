52d12188a3f53eda124b4996371cfcdd9d55bbaf
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2024-12-26 10:26:59 +0100

    fixed more views

---
+++ view/GameMapView.java
@@ -0,0 +1,16 @@
+package view;
+import javax.swing.JPanel;
+
+import view.MainView;
+
+
+public class GameMapView {
+    public GameMapView(){
+
+    }
+    
+    public void createGameMapGUI(String[] gameMapRepresentation){
+        JPanel gameMapGUI = new JPanel();
+        
+    }
+}
