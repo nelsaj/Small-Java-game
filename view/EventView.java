5e60065a4cee3c5fea09c28e54e81e3ff324345f
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2024-12-29 21:46:22 +0100

    Made the event list GUI, this means the GUI layout is done

--- view/EventView.java
+++ view/EventView.java
@@ -1,11 +1,30 @@
 package view;
 
 import java.awt.Event;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
 
 public class EventView implements MessageViewsInterface {
     private MainView mainView;
     public EventView(MainView mainView){
         this.mainView = mainView;
+        createEventViewGUI();
     }
+    
     public void showMessage (String message) {};
+
+    public void createEventViewGUI(){
+        JList eventListGUI = new JList<>();
+        JLabel eventListGUILabel = new JLabel("Event list");
+        eventListGUILabel.setBounds(550, 0, 100, 50); 
+        eventListGUILabel.setFont(new Font("serif", Font.PLAIN, 18));
+        
+        eventListGUI.setBounds(550, 80, 300, 450);
+        mainView.getWindowDrawSpace().add(eventListGUI);
+        mainView.getWindowDrawSpace().add(eventListGUILabel);
+    }
 }
